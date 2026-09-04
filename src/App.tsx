a3bb2f973b7dc345345ca6b28343afaee8ccb9cc
Sam Wilson <lifeofthecoders@gmail.com>
2026-09-04 12:40:21 +0530

    Fix unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
 import { ProjectsSection } from './components/ProjectsSection';
